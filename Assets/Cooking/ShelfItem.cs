c0d245eca7089d7109ff4580077083d267d733a5
JAJUa <127952072+JAJUa@users.noreply.github.com>
2024-01-07 07:42:56 +0900

    ScenePeristalsis

--- Assets/Cooking/ShelfItem.cs
+++ Assets/Cooking/ShelfItem.cs
@@ -1,11 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ShelfItem : MonoBehaviour
 {
     public bool OnItem;
+    public bool SubmitTable;
+    public GameManager gameManager;
 
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Material"))
@@ -13,12 +22,35 @@ public class ShelfItem : MonoBehaviour
             OnItem = true;
         }
     }
-
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Material"))
         {
             OnItem = false;
         }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        //submit?
+        if (SubmitTable)
+        {
+            HoldAndDropFood holdAndDropFood = other.GetComponent<HoldAndDropFood>();
+            if (holdAndDropFood.isInLine && !holdAndDropFood.isBeingHeld)
+            {
+                FinalFood finalFood = other.GetComponent<FinalFood>();
+                if (finalFood != null)
+                {
+                    Debug.Log("TEST3");
+                    gameManager.AddToMakedFood(finalFood.makedFood);
+                    Destroy(other.gameObject);
+                }
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
     }
 }
