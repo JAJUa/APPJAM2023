e7fab67ce2ee040978fbd7388b4c5df2ebb0436a
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2024-01-06 18:22:36 +0900

    게임매니저 생성

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if(_instance != null)
+            {
+                return _instance;
+            }
+
+            GameObject g = new GameObject("SoundManager");
+            _instance = g.AddComponent<GameManager>();
+            DontDestroyOnLoad(g);
+            return _instance;
+        }
+    }
+}
