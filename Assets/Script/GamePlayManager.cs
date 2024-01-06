eb0911f6afcbeef05d797863088d1657a64cf32e
Kang JiUn <46744270+rkdwldns00@users.noreply.github.com>
2024-01-06 19:32:36 +0900

    날짜 전환 연출

--- Assets/Script/GamePlayManager.cs
+++ Assets/Script/GamePlayManager.cs
@@ -2,21 +2,53 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GamePlayManager : MonoBehaviour
 {
-    public int curruntDay { get; private set; } = 1;
+    [Header("날짜 전환 연출")]
+    [SerializeField] private float _dayChangeFadingTime;
+    [SerializeField] private FadeInOut _fadeInOut;
+    [SerializeField] private Text _dayUIText;
+    [SerializeField] private FadeInOut _dayUIfade;
+
+    public int curruntDay { get; private set; }
+
+    private void Start()
+    {
+        _fadeInOut.SetFade(true);
+        StartDay();
+    }
 
     public event Action OnFinishedDay;
     public void FinishDay()
     {
         OnFinishedDay?.Invoke();
+
+        _fadeInOut.FadeIn(_dayChangeFadingTime);
+
+        StartCoroutine(Delay());
+        IEnumerator Delay()
+        {
+            yield return new WaitForSeconds(_dayChangeFadingTime);
+            StartDay();
+        }
     }
 
     public event Action OnStartedDay;
-    public void StartDay()
+    private void StartDay()
     {
         curruntDay++;
         OnStartedDay?.Invoke();
+
+        _dayUIfade.FadeIn(_dayChangeFadingTime);
+
+        StartCoroutine(Delay());
+        IEnumerator Delay()
+        {
+            yield return new WaitForSeconds(_dayChangeFadingTime + 1);
+            _dayUIfade.FadeOut(_dayChangeFadingTime);
+            _fadeInOut.FadeOut(_dayChangeFadingTime);
+        }
     }
 }
--- Assets/Script/GamePlayUI/FadeInOut.cs
+++ Assets/Script/GamePlayUI/FadeInOut.cs
@@ -5,19 +5,34 @@ using UnityEngine.UI;
 
 public class FadeInOut : MonoBehaviour
 {
-    [SerializeField] private Image _image;
+    [SerializeField] private Graphic _graphic;
 
-    private bool _isBlack;
+    private bool _isShow;
     private float _fadingTime;
     private float _remainingFadeTime;
+
+    private void Awake()
+    {
+        _graphic.enabled = false;
+    }
 
     private void Update()
     {
         if (_remainingFadeTime > 0)
         {
             _remainingFadeTime -= Time.deltaTime;
-            Color c = _image.color;
-            if (_isBlack)
+
+            if (_remainingFadeTime <= 0)
+            {
+                _graphic.enabled = _isShow;
+            }
+            else
+            {
+                _graphic.enabled = true;
+            }
+
+            Color c = _graphic.color;
+            if (_isShow)
             {
                 c.a = 1 - _remainingFadeTime / _fadingTime;
             }
@@ -25,21 +40,48 @@ public class FadeInOut : MonoBehaviour
             {
                 c.a = _remainingFadeTime / _fadingTime;
             }
-            _image.color = c;
+            _graphic.color = c;
         }
     }
 
+    /// <summary>
+    /// 암전
+    /// </summary>
     public void FadeIn(float fadingTime)
     {
-        _isBlack = true;
+        _isShow = true;
         _fadingTime = fadingTime;
         _remainingFadeTime = _fadingTime;
     }
 
+    /// <summary>
+    /// 복원
+    /// </summary>
     public void FadeOut(float fadingTime)
     {
-        _isBlack = false;
+        _isShow = false;
         _fadingTime = fadingTime;
         _remainingFadeTime = _fadingTime;
+    }
+
+    public void SetFade(bool isShow)
+    {
+        if (!isShow)
+        {
+            _graphic.enabled = false;
+            return;
+        }
+
+        _graphic.enabled = true;
+        Color c = _graphic.color;
+        if (isShow)
+        {
+            c.a = 1;
+        }
+        else
+        {
+            c.a = 0;
+        }
+        _graphic.color = c;
     }
 }
